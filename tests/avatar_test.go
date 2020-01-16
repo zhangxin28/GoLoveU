26ba55c9142331cf67d17545eb9697362e455fee
Simon <zengweiking@outlook.com>
2020-01-16 11:06:45 +0800

    add a test case for avatar.Generate

--- tests/avatar_test.go
+++ tests/avatar_test.go
@@ -18,6 +18,10 @@ func TestGenerate(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
+		{
+			name: "1",
+			args: args{uid: 1234},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
