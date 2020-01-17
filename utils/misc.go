b9296b8e95c6ba610a1219717fb0afc757f7c28a
Simon <zengweiking@outlook.com>
2020-01-17 17:47:03 +0800

    add misc.go to add misc functions

---
+++ utils/misc.go
@@ -0,0 +1,51 @@
+package utils
+
+import (
+	"reflect"
+	"strings"
+)
+
+// IsEmpty shows whether a valus is empty
+func IsEmpty(a interface{}) bool {
+	v := reflect.ValueOf(a)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	return v.Interface() == reflect.Zero(v.Type()).Interface()
+}
+
+// Contains indicates whether the target contains the search
+func Contains(search interface{}, target interface{}) bool {
+	targetValue := reflect.ValueOf(target)
+	switch reflect.TypeOf(target).Kind() {
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < targetValue.Len(); i++ {
+			if targetValue.Index(i).Interface() == search {
+				return true
+			}
+		}
+	case reflect.Map:
+		if targetValue.MapIndex(reflect.ValueOf(search)).IsValid() {
+			return true
+		}
+	}
+	return false
+}
+
+// ContainsIgnoreCase represents the target contains the search, ignore case
+func ContainsIgnoreCase(search string, target []string) bool {
+	if len(search) == 0 {
+		return false
+	}
+
+	if len(target) == 0 {
+		return false
+	}
+	search = strings.ToLower(search)
+	for i := 0; i < len(target); i++ {
+		if strings.ToLower(target[i]) == search {
+			return true
+		}
+	}
+	return false
+}
